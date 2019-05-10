1e58c68fe01a30a71a8dd3dd5634a6a389cca2f1
ISMAIL UMAR <43749027+ismaild0217308@users.noreply.github.com>
2019-05-10 10:57:44 +0800

    Create STARWARS0005.java

---
+++ STARWARS0005.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+//NAMA : ISMAIL UMAR
+//NIM  : D0217308
+//KELAS : INFORMATIKA. A 2017
+
+public class STARWARS0005 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Input A : ");
+		int a = sc.nextInt();
+		
+		System.out.print("Input B : ");
+		int b = sc.nextInt();
+		
+		for (int i = 0; i <a;i++){
+			for (int j = 0; j<i;j++){
+		}
+		for(int j=0;j<b;j++){
+			System.out.print("x");
+			
+		}
+		System.out.println();
+		}	
+	}
+
+}
